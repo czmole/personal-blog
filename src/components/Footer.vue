1b1b634dfe3a043937fbdc7c944a2ff94ae06a51
Catalin Zmole <catalin@zmole.ro>
2020-02-19 21:52:31 +0200

    footer change and google webmaster tools verification

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="text-gray-700 text-sm leading-normal flex flex-wrap justify-between mx-auto max-w-3xl px-6 sm:px-12">
     <div class="w-full sm:w-1/2 mb-4 sm:mb-0">
       <p><a href="https://github.com/cossssmin/gridsome-starter-bleda" class="border-b border-transparent hover:border-gray-400 transition-border-color">Bleda</a> by <a href="https://twitter.com/cossssmin" class="border-b border-transparent hover:border-gray-400 transition-border-color">@cossssmin</a></p>
-      <p>Built with <a href="https://gridsome.org" class="border-b border-transparent hover:border-gray-400 transition-border-color">Gridsome</a> &amp; <a href="https://tailwindcss.com/" class="border-b border-transparent hover:border-gray-400 transition-border-color">Tailwind CSS</a>. Hosted on <a href="https://netlify.com" target="_blank">netlify.com</a></p>
+      <p>Built with <a href="https://gridsome.org" class="border-b border-transparent hover:border-gray-400 transition-border-color">Gridsome</a> &amp; <a href="https://tailwindcss.com/" class="border-b border-transparent hover:border-gray-400 transition-border-color">Tailwind CSS</a>.<br>Hosted on <a href="https://netlify.com" target="_blank" class="border-b border-transparent hover:border-gray-400 transition-border-color">netlify.com</a></p>
     </div>
     <div class="w-full sm:w-1/2">
       <nav>
